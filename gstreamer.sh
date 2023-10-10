c4d4fdf9124e3b6ee02a0a8957d018cbc75d99b4
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-10 15:37:24 -0800

    flip gstreamer video

--- gstreamer.sh
+++ gstreamer.sh
@@ -1,3 +1,3 @@
 TARGET_IP=192.168.40.4
 TARGET_PORT=5600
-gst-launch-1.0 --gst-plugin-path=install/gst_bridge/lib/gst_bridge/ rosimagesrc ros-topic=/camera/infra1/image_rect_raw ! queue max-size-buffers=1 ! video/x-raw,format=GRAY8 ! videoconvert ! x264enc bitrate=2000 tune=zerolatency speed-preset=ultrafast ! video/x-h264,stream-format=byte-stream ! rtph264pay config-interval=1 pt=96 ! udpsink host=$TARGET_IP port=$TARGET_PORT sync=false
+gst-launch-1.0 --gst-plugin-path=install/gst_bridge/lib/gst_bridge/ rosimagesrc ros-topic=/camera/infra1/image_rect_raw ! queue max-size-buffers=1 ! videoflip method=rotate-180 ! video/x-raw,format=GRAY8 ! videoconvert ! x264enc bitrate=2000 tune=zerolatency speed-preset=ultrafast ! video/x-h264,stream-format=byte-stream ! rtph264pay config-interval=1 pt=96 ! udpsink host=$TARGET_IP port=$TARGET_PORT sync=false
