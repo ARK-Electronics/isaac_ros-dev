ed41bf92c76a192e04f5eba65ff81d8b0716ff2b
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-12 15:36:09 -0800

    detach docker exec

--- start_docker_containers.sh
+++ start_docker_containers.sh
@@ -154,10 +154,10 @@ docker run \
 
 # Attach to running container
 echo "Attaching to running container"
-docker exec -u admin --workdir /workspaces/isaac_ros-dev $CONTAINER_NAME /workspaces/isaac_ros-dev/vslam_realsense.sh
+docker exec -d -u admin --workdir /workspaces/isaac_ros-dev $CONTAINER_NAME /workspaces/isaac_ros-dev/vslam_realsense.sh
 
 # Attach to running container
 echo "Attaching to running container"
-docker exec -u admin --workdir /workspaces/isaac_ros-dev $CONTAINER_NAME /workspaces/isaac_ros-dev/foxglove.sh
+docker exec -d -u admin --workdir /workspaces/isaac_ros-dev $CONTAINER_NAME /workspaces/isaac_ros-dev/foxglove.sh
 
 echo "Containers started, exiting."
