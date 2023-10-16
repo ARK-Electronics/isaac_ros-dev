aa1c1f7e3ad84525800eec23f07ba25c536c5ee5
Jacob Dahl <dahl.jakejacob@gmail.com>
2023-10-15 22:17:52 -0800

    updated dockerfile, updated settime.sh with ip/port

--- settime.sh
+++ settime.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+HOST_IP=127.0.0.1
+HOST_PORT=14569
+
 MY_PATH=$(dirname "$0")
 UNIX_EPOCH_TIME=$(date +%s) # %s = seconds since the Epoch (1970-01-01 00:00 UTC)
-$MY_PATH/px4_shell_command.py -p "system_time set $UNIX_EPOCH_TIME"
+$MY_PATH/px4_shell_command.py -p "udp:$HOST_IP:$HOST_PORT" "system_time set $UNIX_EPOCH_TIME"
